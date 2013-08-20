42f031a2c983e31d07cc1c5bef408b76788ba939
aleafs <zhangxc83@gmail.com>
2013-08-20 16:29:24 +0800

    Add test case

--- test/jmath.test.js
+++ test/jmath.test.js
@@ -12,18 +12,30 @@ describe('jmath interface', function () {
   it('should_array_sum_avg_works_fine', function () {
     jmath.sum([1, 2, 3]).should.eql(6);
     jmath.sum([]).should.eql(0);
-    (function () {
-      jmath.sum('a');
-    }).should.throwError('ArrayRequired');
 
     jmath.avg([]).should.eql(0);
     jmath.avg([1, 2, 3]).should.eql(2);
+
+    try {
+      jmath.sum('a');
+      (true).should.eql(false);
+    } catch (e) {
+      e.message.should.include('ArrayRequired');
+    }
   });
   /* }}} */
 
+  /* {{{ should_linear_regression_works_fine() */
   it('should_linear_regression_works_fine', function () {
     jmath.linearRegression([0, 1, 2], [1, 3, 5]).should.eql([2, 1]);
+    try {
+      jmath.linearRegression([0, 1], [2]);
+      (true).should.eql(false);
+    } catch (e) {
+      e.message.should.include('ArraySizeNotEqual');
+    }
   });
+  /* }}} */
 
 });
 
